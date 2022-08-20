2482d7c07e548ae373eb854d98d338212376d7e4
Sarthak Shukla <sarthakshukla72@gmail.com>
2022-08-20 19:43:25 +0530

    added CartItem page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import React from "react";
+import CartItem from "./CartItem";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Cart</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <CartItem />
+        </div>
+    );
 }
 
 export default App;
