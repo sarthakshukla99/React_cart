753ef816c6e37166f1599f64a100ab06dbe940b7
Sarthak Shukla <sarthakshukla72@gmail.com>
2022-08-22 14:44:23 +0530

    added props in cartitem

--- src/Cart.js
+++ src/Cart.js
@@ -2,15 +2,49 @@ import React from "react";
 import CartItem from "./CartItem";
 
 class Cart extends React.Component {
-    render(){
-        return(
-            <div className="cart">
-                <CartItem />
-                <CartItem />
-                <CartItem />
-            </div>
-        )
+    constructor() {
+        super();
+        this.state = {
+            products: [
+                {
+                    price: 99,
+                    title: "Watch",
+                    qty: 1,
+                    img: "",
+                    id: 1,
+                },
+                {
+                    price: 999,
+                    title: "Mobile Phone",
+                    qty: 8,
+                    img: "",
+                    id: 2,
+                },
+                {
+                    price: 22999,
+                    title: "Laptop",
+                    qty: 4,
+                    img: "",
+                    id: 3,
+                },
+            ],
+        };
     }
-};
+    render() {
+        const { products } = this.state;
+        return (
+            <div className="cart">
+                {products.map((product) => {
+                    return (
+                        <CartItem 
+                        product={product} 
+                        key={product.id} 
+                        />
+                    )
+                })}
+            </div>
+        );
+    }
+}
 
 export default Cart;
--- src/CartItem.js
+++ src/CartItem.js
@@ -3,12 +3,6 @@ import React from "react";
 class CartItem extends React.Component {
     constructor() {
         super();
-        this.state = {
-            price: 999,
-            title: "Mobile Phone",
-            qty: 1,
-            img: "",
-        };
     }
 
     increaseQuantity= ()=>{
@@ -34,7 +28,8 @@ class CartItem extends React.Component {
     }
 
     render() {
-        const { price, title, qty } = this.state;
+        const { price, title, qty } = this.props.product;
+        console.log('this.props', this.props)
         return (
             <div className="cart-item">
                 <div className="left-block">
